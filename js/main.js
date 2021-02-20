36ccf02d6965f0603f32d197ebc22e8560844a91
Sergio Victor Nowosad Petruk <sergiovnp@gmail.com>
2021-02-19 22:36:32 -0300

    Ejemplo de jQuery

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-var elemento = document.querySelector('h1');
-console.log(elemento.innerHTML);
+// var elemento = document.querySelector('h1');
+// console.log(elemento.innerHTML);
 
+// var timeout = setTimeout(function(){
+//     elemento.innerHTML='Hola desde JavaScript!';
+// }, 1000)
+
+
+var elemento =jQuery('h1');
+// console.log(elemento.text());
 var timeout = setTimeout(function(){
-    elemento.innerHTML='Hola desde JavaScript!';
-}, 1000)
+    elemento.text('Ahora si es Jquery!');
+    console.log(elemento.text());
+}, 1000);
