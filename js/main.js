f037907483ae6394c9e1f3fe90b1e829ee57a056
Sergio Victor Nowosad Petruk <sergiovnp@gmail.com>
2021-02-20 22:55:23 -0300

    Segunda clase - JavaScript - preventDefault

--- js/main.js
+++ js/main.js
@@ -6,9 +6,11 @@
 // }, 1000)
 
 
-var elemento =jQuery('h1');
-// console.log(elemento.text());
-var timeout = setTimeout(function(){
-    elemento.text('Ahora si es Jquery!');
-    console.log(elemento.text());
-}, 1000);
+// var elemento =jQuery('h1');
+// // console.log(elemento.text());
+// var timeout = setTimeout(function(){
+//     elemento.text('Ahora si es Jquery!');
+//     console.log(elemento.text());
+// }, 1000);
+
+
